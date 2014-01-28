955d9a402c113fcd0e1f1e13aee9d3e57fd5581f
Noah Kantrowitz <noah@coderanger.net>
2014-01-27 22:12:34 -0800

    Deprecate this. Might need it some day, but not for now.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,20 @@
 #
-# Cookbook Name:: balanced-python
-# Recipe:: default
+# Author:: Noah Kantrowitz <noah@coderanger.net>
 #
-# Copyright 2013, Balanced
+# Copyright 2013-2014, Balanced, Inc.
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-case node[:platform_family]
-  when "debian"
-    include_recipe 'apt'
-end
-
-include_recipe "python::#{node['python']['install_method']}"
-include_recipe 'balanced-python::_common'
+Chef::Log.warn('The balanced-python cookbook is deprecated, please just use python')
+include_recipe 'python'
